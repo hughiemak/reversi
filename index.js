13faa8fbe7fc091c09dfcb02507bdb74ca5364d1
hughiemak <hughiemak@users.noreply.github.com>
2019-06-08 18:40:31 +0800

    live update scores

--- index.js
+++ index.js
@@ -11,7 +11,15 @@ var gameState =
         [null, null, null, null, null, null, null, null],
     ];
 
+var gameTurn = 0;
+
+var noOfWhite;
+var noOfBlack;
+
 function onload() {
+
+    var boardContainer = $(".board-container")
+    boardContainer.hide();
 
     for (var y = 7; y >= 0; y--) {
         for (var x = 0; x <= 7; x++) {
@@ -28,23 +36,45 @@ function onload() {
                 var x = element.data("position").x
                 var y = element.data("position").y
                 placeChessOn(x, y)
-                element.unbind();
+                switchGameTurn()
             })
         }
     }
 
-    resetGameState()
+    initGameState()
 }
 
-function resetGameState() {
+function displayChessScores(){
+    displayNoOfWhite()
+    displayNoOfBlack()
+}
+
+function displayNoOfWhite(){
+    var element = $(".whiteScore")
+    element.html(noOfWhite);
+
+}
+
+function displayNoOfBlack(){
+    var element = $(".blackScore")
+    element.html(noOfBlack);
+}
+
+function switchGameTurn(){
+    if (gameTurn == 0) {
+        gameTurn = 1
+    }else{
+        gameTurn = 0
+    }
+}
+
+function initGameState() {
     //33, 44 black
     placeBlackChessOn(3, 3)
     placeBlackChessOn(4, 4)
     //34, 43 white
     placeWhiteChessOn(3, 4)
     placeWhiteChessOn(4, 3)
-
-
 }
 
 function renderSqaure(x, y) {
@@ -57,24 +87,53 @@ function getSquare(x, y) {
     return element
 }
 
-function placeChessOn(x, y, square) {
-    getSquare(x, y).append('<div class="chess"></div>')
-    gameState[y][x] = 0;
-    console.log("gameState: " + gameState);
+function placeChessOn(x, y) {
+    if (gameTurn == 0){
+        placeWhiteChessOn(x, y)
+    }else{
+        placeBlackChessOn(x, y)
+    }
 }
 
 function placeWhiteChessOn(x, y) {
-    getSquare(x, y).append('<div class="whiteChess"></div>')
+    var element = getSquare(x, y)
+    element.append('<div class="whiteChess"></div>')
     // square.append('<div class="chess"></div>')
-    gameState[y][x] = -1;
+    gameState[y][x] = 0;
     console.log("gameState: " + gameState);
+    element.unbind();
+    updateChessCount()
 }
 
 function placeBlackChessOn(x, y) {
-    getSquare(x, y).append('<div class="blackChess"></div>')
+    var element = getSquare(x, y)
+    element.append('<div class="blackChess"></div>')
     // square.append('<div class="chess"></div>')
     gameState[y][x] = 1;
     console.log("gameState: " + gameState);
+    element.unbind();
+    updateChessCount()
+}
+
+function updateChessCount(){
+    var whiteCount = 0
+    var blackCount = 0
+
+    gameState.forEach(element => {
+        element.forEach(element => {
+            if (element == 1) {
+                whiteCount += 1
+            }else if (element == 0){
+                blackCount += 1
+            }
+        });
+    });
+
+    noOfBlack = blackCount;
+    noOfWhite = whiteCount;
+
+    console.log("noOfBlack: " + noOfBlack + ", noOfWhite: " + noOfWhite)
+    displayChessScores()
 }
 
 window.onload = onload();

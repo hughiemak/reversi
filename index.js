d13a588f6ae3c4ef22d497177bd24cc68bf6b996
hughiemak <hughiemak@users.noreply.github.com>
2019-06-08 17:06:11 +0800

    init game

--- index.js
+++ index.js
@@ -1,41 +1,80 @@
 
-
+var gameState =
+    [
+        [null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null],
+    ];
 
 function onload() {
 
-    for(var i = 7; i >= 0; i--){
-        for(var j = 0; j<= 7; j++){
+    for (var y = 7; y >= 0; y--) {
+        for (var x = 0; x <= 7; x++) {
 
-            renderSqaure(i, j)
+            renderSqaure(x, y)
 
-            let square = getSquare(i, j)
+            let square = getSquare(x, y)
 
-            square.data("position", {x: i, y: j})
+            square.data("position", { x: x, y: y })
 
-            square.click(function(event){
+            square.click(function (event) {
                 var element = $("#" + event.target.id)
-                console.log("x: " + element.data("position").x + ", y: " + element.data("position").y)
-                displayChessOn(element)
+                // console.log("x: " + element.data("position").x + ", y: " + element.data("position").y)
+                var x = element.data("position").x
+                var y = element.data("position").y
+                placeChessOn(x, y)
                 element.unbind();
             })
         }
     }
 
+    resetGameState()
+}
+
+function resetGameState() {
+    //33, 44 black
+    placeBlackChessOn(3, 3)
+    placeBlackChessOn(4, 4)
+    //34, 43 white
+    placeWhiteChessOn(3, 4)
+    placeWhiteChessOn(4, 3)
+
 
 }
 
-function renderSqaure(x, y){
+function renderSqaure(x, y) {
     var selectorString = 'squareId_' + x + '' + y;
     $('#squares-container').append('<div id="' + selectorString + '" class="square"></div>')
 }
 
-function getSquare(x, y){
+function getSquare(x, y) {
     var element = $("#" + "squareId_" + x + "" + y)
     return element
 }
 
-function displayChessOn(square){
-    square.append('<div class="chess"></div>')
+function placeChessOn(x, y, square) {
+    getSquare(x, y).append('<div class="chess"></div>')
+    gameState[y][x] = 0;
+    console.log("gameState: " + gameState);
+}
+
+function placeWhiteChessOn(x, y) {
+    getSquare(x, y).append('<div class="whiteChess"></div>')
+    // square.append('<div class="chess"></div>')
+    gameState[y][x] = -1;
+    console.log("gameState: " + gameState);
+}
+
+function placeBlackChessOn(x, y) {
+    getSquare(x, y).append('<div class="blackChess"></div>')
+    // square.append('<div class="chess"></div>')
+    gameState[y][x] = 1;
+    console.log("gameState: " + gameState);
 }
 
 window.onload = onload();
